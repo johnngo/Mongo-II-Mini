a15a4b8131fa787eef9f6d10124b20ad3f78a747
John <johnngo@Johns-MacBook-Pro.local>
2018-05-08 12:25:04 -0700

    added GET, GET and PUT

--- server.js
+++ server.js
@@ -16,6 +16,47 @@ server.get('/', function(req, res) {
   res.status(200).json({ api: 'running' });
 });
 
+server.get('/users/:direction', (req, res) => {
+  const { direction } = req.params; //asc desc
+  Person.find({})
+    .sort({ firstName: direction })
+    .exec((err, sortedUsers) => {
+      if (err) {
+        res.status(422).json({ 'Error getting/sorting your users: ': err });
+        return;
+      }
+      res.json(sortedUsers);
+    });
+});
+
+server.get('/users-get-friends/:id', (req, res) => {
+  const { id } = req.params;
+  Person.findById(id)
+    .select('friends')
+    .exec((err, friends) => {
+      if (err) {
+        res.status(422).json({ 'Could not find that id: ': err });
+        return;
+      }
+      res.json(friends);
+    });
+});
+
+server.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName } = req.body;
+
+  Person.findByIdAndUpdate( id, { firstName, lastName }, { new: true }).exec(
+    (err, updatedUser) => {
+      if (err) {
+        res.status(422).json({ 'Could not find that id: ': err });
+        return;
+      }
+      res.json(updatedUser);
+    }
+  );
+});
+
 mongoose.connect('mongodb://localhost/library').then(
   () => {
     const port = process.env.PORT || 3000;
